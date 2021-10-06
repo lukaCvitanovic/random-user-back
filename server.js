a3c5d4a11c1c006f5e2e4e7d4809314201724a49
lukaCvitanovic <lcvita01@fesb.hr>
2021-10-07 00:46:47 +0200

    Added basic express server - Added eslint

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const http = require('http');
-const hostname = 'localhost';
-const port = 3000;
+const express = require('express');
+const app = express();
+const port = 7000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Server is listening on posrt ${port}`);
 });
